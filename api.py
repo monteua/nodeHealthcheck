8d217dff7d1944c712adee99f82646273fda5254
monteua <vadym.stavskyi@gmail.com>
2021-04-10 20:44:27 +0300

    Node monitoring script (with Telegram notifications)

---
+++ api.py
@@ -0,0 +1,108 @@
+from abc import ABC
+
+import requests
+import time
+from decouple import config
+
+nodes = dict()
+timeout = 300  # seconds
+last_updated = time.time()  # when the last API request was sent
+
+
+class API(ABC):
+
+    def __init__(self):
+        self.endpoint = "https://nodes.presearch.org/api/nodes/status/" + config('API_KEY')
+
+    def get_update_from_api(self):
+        global nodes, timeout, last_updated
+        
+        if len(nodes) == 0 or time.time() - last_updated > timeout:
+            print("Sending the api request")
+            nodes = requests.get(self.endpoint).json()['nodes']
+            last_updated = time.time()
+
+    def get_node_list(self):
+        global nodes
+
+        self.get_update_from_api()
+        return [nodes[node]['meta']['description'] for node in nodes]
+
+    def get_status_for_nodes(self):
+        global nodes
+        
+        response = list()
+        msg = """{status} {description} | {gateway_pool} | v{version}"""
+
+        self.get_update_from_api()
+        for node in nodes:
+            description = nodes[node]['meta']['description']
+            gateway_pool = nodes[node]['meta']['gateway_pool']
+            version = nodes[node]['meta']['version']
+            status_connected = nodes[node]['status']['connected']
+
+            response.append(msg.format(
+                description=description,
+                status="\U0001F7E2" if status_connected else "\U0001F534",
+                version=version,
+                gateway_pool=gateway_pool
+            ))
+
+        return "\n".join(response)
+
+    def get_status_for_node(self, node_description):
+        global nodes
+
+        msg = """Name: {description}\nURL: {url}\nServer Description: {server_description} \
+        \nServer Url: {server_url}\nGateway Pool: {gateway_pool}\nRemote Address: {remote_addr}\nVersion: {version} \
+        \nConnected: {status_connected} ({status_minutes_in_current_state})\nBlocked: {status_blocked} \
+        \nIn current state since: {status_in_current_state_since}"""
+
+        self.get_update_from_api()
+        for node in nodes:
+            if nodes[node]['meta']['description'] == node_description:
+                description = nodes[node]['meta']['description']
+                url = nodes[node]['meta']['url']
+                server_description = nodes[node]['meta']['server_description']
+                server_url = nodes[node]['meta']['server_url']
+                gateway_pool = nodes[node]['meta']['gateway_pool']
+                remote_addr = nodes[node]['meta']['remote_addr']
+                version = nodes[node]['meta']['version']
+                status_connected = nodes[node]['status']['connected']
+                status_blocked = nodes[node]['status']['blocked']
+                status_in_current_state_since = nodes[node]['status']['in_current_state_since']
+                status_minutes_in_current_state = nodes[node]['status']['minutes_in_current_state']
+
+                day = int(int(status_minutes_in_current_state) / 1440)
+                hour = int(int(status_minutes_in_current_state) % 1440 / 60)
+                minutes = (int(status_minutes_in_current_state) - day * 1440 - hour * 60)
+
+                return msg.format(
+                    description=description,
+                    url=url,
+                    server_description=server_description,
+                    server_url=server_url,
+                    gateway_pool=gateway_pool,
+                    remote_addr=remote_addr,
+                    version=version,
+                    status_connected="\U0001F7E2" if status_connected else "\U0001F534",
+                    status_minutes_in_current_state=str(day) + "d " + str(hour) + "h " + str(minutes) + "m",
+                    status_blocked="Yes" if status_blocked else "No",
+                    status_in_current_state_since=status_in_current_state_since
+                )
+
+    def health_check(self):
+        global nodes
+
+        response = list()
+        msg = """{status} {description}"""
+
+        self.get_update_from_api()
+        for node in nodes:
+            description = nodes[node]['meta']['description']
+            status_connected = nodes[node]['status']['connected']
+
+            if not status_connected:
+                response.append(msg.format(description=description, status="\U0001F534"))
+
+        return response
---
+++ bot.py
@@ -0,0 +1,109 @@
+import logging
+import telegram
+from decouple import config
+from telegram import ParseMode, Update, InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton
+from telegram.ext import Updater, CommandHandler, CallbackContext, CallbackQueryHandler, Filters
+
+from api import API
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+chat_id = str()
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def send_keyboard(update, context):
+    keyboard = [[KeyboardButton('/nodes'), KeyboardButton('/manage'), KeyboardButton('/watchdog')]]
+    resize_keyboard = True
+    one_time_keyboard = True
+    selective = True
+
+    json_dict = {
+        'keyboard': [[keyboard[0][0].to_dict(), keyboard[0][1].to_dict()]],
+        'resize_keyboard': resize_keyboard,
+        'one_time_keyboard': one_time_keyboard,
+        'selective': selective,
+    }
+
+    message = update.message.reply_text(
+        "Make your selection",
+        reply_markup=telegram.ReplyKeyboardMarkup(keyboard, update))
+
+
+def send_nodes_status(update, context):
+    update.message.reply_text(API().get_status_for_nodes(), parse_mode=ParseMode.HTML, disable_web_page_preview=True)
+
+
+def get_node_details(update, context):
+    keyboard = []
+
+    for node_name in API().get_node_list():
+        keyboard.append([InlineKeyboardButton(text=node_name, callback_data=node_name)])
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    try:
+        update.callback_query.edit_message_text('please select the node',
+                                                reply_markup=reply_markup)
+    except Exception:
+        update.message.reply_text('please select the node',
+                                  reply_markup=reply_markup)
+
+
+def button(update: Update, _: CallbackContext) -> None:
+    query = update.callback_query
+    query.answer()
+
+    keyboard = [[InlineKeyboardButton(text="< Back", callback_data="back")]]
+
+    if query.data == "back":
+        get_node_details(update, "getUpdates")
+        return
+
+    query.edit_message_text(text=API().get_status_for_node(query.data), reply_markup=InlineKeyboardMarkup(keyboard))
+
+
+def health_check(context):
+    response = API().health_check()
+
+    if len(response) > 0:
+        context.bot.send_message(chat_id=chat_id,
+                                 text="\n".join(response))
+
+
+def run_watch_dog(update, context):
+    global chat_id
+
+    chat_id = update.message.chat_id
+    context.bot.send_message(chat_id=update.message.chat_id,
+                             text="Watching for node status change")
+
+    context.job_queue.run_repeating(health_check, interval=300, first=1,
+                                    context=update.message.chat_id)
+
+
+def main():
+    updater = Updater(config('TELEGRAM_API_KEY'), use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler('start', send_keyboard, Filters.user(username=config('ADMIN_ID'))))
+    dp.add_handler(CommandHandler('nodes', send_nodes_status, Filters.user(username=config('ADMIN_ID'))))
+    dp.add_handler(CommandHandler('manage', get_node_details, Filters.user(username=config('ADMIN_ID'))))
+    dp.add_handler(CallbackQueryHandler(button))
+    dp.add_handler(
+        CommandHandler('watchdog', run_watch_dog, Filters.user(username=config('ADMIN_ID')), pass_job_queue=True))
+
+    dp.add_error_handler(error)
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
